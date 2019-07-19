18b84a9d415657b7595e4d8dd7aa479096ee7833
YuJaeHyun <slade12307@gmail.com>
2019-07-19 11:53:22 +0900

    /app/src/main/java/com/easyfitness/dao에서 DAOProfil.java 프로필 관련 정리

--- app/src/main/java/com/easyfitness/DAO/DAOProfil.java
+++ app/src/main/java/com/easyfitness/DAO/DAOProfil.java
@@ -18,16 +18,16 @@ public class DAOProfil extends DAOBase {
     // Contacts table name
     public static final String TABLE_NAME = "EFprofil";
 
-    public static final String KEY = "_id";
-    public static final String NAME = "name";
-    public static final String CREATIONDATE = "creationdate";
-    public static final String SIZE = "size";
-    public static final String BIRTHDAY = "birthday";
-    public static final String PHOTO = "photo";
-    public static final String GENDER = "gender";
+    public static final String KEY = "_id";                             // 아이디를 통해 사용자 프로필 구분 하는 듯(KEY라는 변수 이용)
+    public static final String NAME = "name";                           // NAME = 이름 변수
+    public static final String CREATIONDATE = "creationdate";       // CREATIONDATE = 생성일 변수
+    public static final String SIZE = "size";                          // SIZE = 사이즈 변수
+    public static final String BIRTHDAY = "birthday";                 // BIRTHDAY = 생일 저장하는 변수
+    public static final String PHOTO = "photo";                         // PHOTO = 사진 저장하는 변수
+    public static final String GENDER = "gender";                       // GENDER = 성별 저장하는 변수
 
     public static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" + KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " + CREATIONDATE + " DATE, " + NAME + " TEXT, " + SIZE + " INTEGER, " + BIRTHDAY + " DATE, " + PHOTO + " TEXT, " + GENDER + " INTEGER);";
-
+    // 사용자 프로필 테이블을 생성하는 문구 생성
     public static final String TABLE_DROP = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
 
     private Cursor mCursor = null;
@@ -45,9 +45,9 @@ public class DAOProfil extends DAOBase {
     public void addProfil(Profile m) {
         // Check if profil already exists
         Profile check = getProfil(m.getName());
-        if (check != null) return;
+        if (check != null) return;          // 체크해서 널값이라면 아무것도 안함
 
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();     // 데이터베이스 SQL LITE 사용하는 것 확인 // SQL LITE DB에 저장
 
         ContentValues value = new ContentValues();
 
@@ -57,8 +57,8 @@ public class DAOProfil extends DAOBase {
         value.put(DAOProfil.SIZE, m.getSize());
         value.put(DAOProfil.PHOTO, m.getPhoto());
         value.put(DAOProfil.GENDER, m.getGender());
-
-        db.insert(DAOProfil.TABLE_NAME, null, value);
+                                                                    // 위와 같이 변수들을 이용해서 데이터들을 PUT해줌
+        db.insert(DAOProfil.TABLE_NAME, null, value);       // DB 에 넣어주는 것을 확인 가능
 
         close();
     }
@@ -80,7 +80,7 @@ public class DAOProfil extends DAOBase {
         //value.put(DAOProfil.BIRTHDAY, DateConverter.dateToDBDateStr(m.getBirthday()));
         //value.put(DAOProfil.SIZE, 0);
 
-        db.insert(DAOProfil.TABLE_NAME, null, value);
+        db.insert(DAOProfil.TABLE_NAME, null, value);           // 얘도 위와 마찬가지 인듯
 
         close();
     }
@@ -88,8 +88,8 @@ public class DAOProfil extends DAOBase {
     /**
      * @param id long id of the Profile
      */
-    public Profile getProfil(long id) {
-        SQLiteDatabase db = this.getReadableDatabase();
+    public Profile getProfil(long id) {                                                     // PROFILE정보가 있다면 IMPORT해오는 기능이 있던데 그것에 관련된 코드      key=?로 되어 있는 걸로 봐서 key값으로 찾거나 새로 입력해주는 듯   creationdate,name,size,birthdaty,photo,gender데이터를 가져와줌
+        SQLiteDatabase db = this.getReadableDatabase();                                        // 아래 구문들은 존재하는 데이터를 가져오는 것들  ( getReadableDatabase() 의미가 데이터베이스를 얻어오는 것이라고 함 )
         if (mCursor != null) mCursor.close();
         mCursor = null;
         mCursor = db.query(TABLE_NAME,
@@ -125,7 +125,7 @@ public class DAOProfil extends DAOBase {
      * @param name String name of the Profile
      */
     public Profile getProfil(String name) {
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase db = this.getReadableDatabase();                             // 얘도 마찬가지로 프로필 데이터를 얻어오는 것  name으로 가져오거나 입력하는 듯  key,creationdate,size,birthday,photo,gender 데이터 가져와줌
         if (mCursor != null) mCursor.close();
         mCursor = null;
         mCursor = db.query(TABLE_NAME,
@@ -158,7 +158,7 @@ public class DAOProfil extends DAOBase {
     }
 
     // Getting All Profils
-    public List<Profile> getProfilsList(String pRequest) {
+    public List<Profile> getProfilsList(String pRequest) {                                  // 모든 것을 그대로 가져오거나 하는듯
         List<Profile> valueList = new ArrayList<>();
         // Select All Query
 
@@ -193,7 +193,7 @@ public class DAOProfil extends DAOBase {
     }
 
     // Getting All Profils
-    public List<Profile> getAllProfils() {
+    public List<Profile> getAllProfils() {                                      //앱에서 프로필 선택하면 profile page가 나타나는데 그거에 대한 리스트 보여주는 것같음 (전부)
         // Select All Query
         String selectQuery = "SELECT  * FROM " + TABLE_NAME + " ORDER BY " + KEY + " DESC";
 
@@ -202,7 +202,7 @@ public class DAOProfil extends DAOBase {
     }
 
     // Getting Top 10 Profils
-    public List<Profile> getTop10Profils() {
+    public List<Profile> getTop10Profils() {                                            //앱에서 프로필 선택하면 profile page가 나타나는데 그거에 대한 리스트 보여주는 것같음 (10개만)
         // Select All Query
         String selectQuery = "SELECT TOP 10 * FROM " + TABLE_NAME + " ORDER BY " + KEY + " DESC";
 
@@ -264,7 +264,7 @@ public class DAOProfil extends DAOBase {
 
     // Updating single value
     public int updateProfile(Profile m) {
-        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();                 // profile 수정할때 사용하는 듯
 
         ContentValues value = new ContentValues();
         value.put(DAOProfil.CREATIONDATE, DateConverter.dateToDBDateStr(m.getCreationDate()));
@@ -282,7 +282,7 @@ public class DAOProfil extends DAOBase {
     // Deleting single Profile
     public void deleteProfil(Profile m) {
         deleteProfil(m.getId());
-    }
+    }               //profile 클릭시 delete구문 나오는데, 이때 사용되는듯
 
     // Deleting single Profile
     public void deleteProfil(long id) {
