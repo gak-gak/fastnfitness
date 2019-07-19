9b14cccfbe73eefb78c8abe7f365f37902815a45
qlslbini <qlslrnldyal5@naver.com>
2019-07-20 00:31:41 +0900

    src/main/java/com/easyfitness/bodymeasures/BodyPartDetailsFragment.java 분석

--- app/src/main/java/com/easyfitness/bodymeasures/BodyPartDetailsFragment.java
+++ app/src/main/java/com/easyfitness/bodymeasures/BodyPartDetailsFragment.java
@@ -40,8 +40,10 @@ import com.onurkaganaldemir.ktoastlib.KToast;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-/**body track 카테고리 내용(측정값 추가, 그래프 등등....)**/
-
+/**
+ * Weight Track, Body Track 두 카테고리에서 나타나는 새로운 측정값 입력이나 클릭반응 ,Delete(Record옆에 X로 나타남),
+ * 그래프 생성, Record Table 생성(그래프 밑에 기록들 나타나는 거) 등등
+**/
 public class BodyPartDetailsFragment extends Fragment {
     Button addButton = null;
     EditText measureEdit = null;
@@ -62,24 +64,25 @@ public class BodyPartDetailsFragment extends Fragment {
         }
     };
     private BtnClickListener itemClickDeleteRecord = this::showDeleteDialog;
-    private OnClickListener onClickAddMeasure = new OnClickListener() {
+    private OnClickListener onClickAddMeasure = new OnClickListener() {  //새로운 데이터 입력하기 위해 해당칸 클릭한 후 반응
         @Override
         public void onClick(View v) {
-            if (!measureEdit.getText().toString().isEmpty()) {
+            if (!measureEdit.getText().toString().isEmpty()) {  //입력한 데이터가 있으면
 
                 Date date = DateConverter.editToDate(dateEdit.getText().toString());
-
+                //새 Edit Value 입력
                 mBodyMeasureDb.addBodyMeasure(date, mBodyPartID, Float.valueOf(measureEdit.getText().toString()), getProfile().getId());
-                refreshData();
-                measureEdit.setText("");
+                refreshData();  //데이터 업데이트
+                measureEdit.setText("");  //Body Track 새 데이터 입력 칸에 새로운 값을 Add하면 칸에 남아있는 글자 없어짐
 
                 Keyboard.hide(getContext(), v);
-            } else {
+            } else {  //입력 데이터가 없으면 경고창 출력
                 KToast.errorToast(getActivity(), "Please enter a measure", Gravity.BOTTOM, KToast.LENGTH_SHORT);
 
             }
         }
     };
+
     private OnItemLongClickListener itemlongclickDeleteRecord = (listView, view, position, id) -> {
 
         // Get the cursor, positioned to the corresponding row in the result set
@@ -164,13 +167,13 @@ public class BodyPartDetailsFragment extends Fragment {
         /* Initialisation des evenements */
 
         // Add the other graph
-        mChart = view.findViewById(R.id.weightChart);
-        mChart.setDescription(null);
+        mChart = view.findViewById(R.id.weightChart);  //완성된 그래프 출력
+        mChart.setDescription(null);  //그래프밑에 세부사항 나타내지 않음.
         mDateGraph = new DateGraph(getContext(), mChart, "");
         mBodyMeasureDb = new DAOBodyMeasure(view.getContext());
 
         // Set Initial text
-        dateEdit.setText(DateConverter.currentDate());
+        dateEdit.setText(DateConverter.currentDate());  //Date 선택 부분에 오늘날짜로 자동 설정
 
         ((MainActivity) getActivity()).getActivityToolbar().setVisibility(View.GONE);
         bodyToolbar.setTitle(getContext().getString(mBodyPart.getResourceNameID()));
@@ -186,7 +189,7 @@ public class BodyPartDetailsFragment extends Fragment {
 
         refreshData();
     }
-
+    /**DrawGraph(), FillRecordTable() 둘 다 void method 인데 후에 거울에 나타나게 하려면 graph랑 record table 따로 변수에 저장해서 가져올 수 있게 코드 수정해야 할듯**/
     private void DrawGraph(List<BodyMeasure> valueList) {  //저장된 값을 이용해 그래프 그리기
 
         // Recupere les enregistrements
@@ -210,8 +213,8 @@ public class BodyPartDetailsFragment extends Fragment {
         mDateGraph.draw(yVals);
     }
 
-    /*  */
-    private void FillRecordTable(List<BodyMeasure> valueList) {  //그래프 아래 나와있는 몸무게 기록 record
+
+    private void FillRecordTable(List<BodyMeasure> valueList) {  //그래프 아래 나와있는 몸무게 기록 record table 만들기
         Cursor oldCursor = null;
 
         if (valueList.isEmpty()) {
@@ -234,25 +237,25 @@ public class BodyPartDetailsFragment extends Fragment {
         return getArguments().getString("name");
     }
 
-    private void refreshData() {
+    private void refreshData() {  /**이 부분 중요**/
         View fragmentView = getView();
         if (fragmentView != null) {
             if (getProfile() != null) {
                 List<BodyMeasure> valueList = mBodyMeasureDb.getBodyPartMeasuresList(mBodyPartID, getProfile());
-                DrawGraph(valueList);
+                DrawGraph(valueList);  //여기서 그래프 생성
                 // update table
-                FillRecordTable(valueList);
+                FillRecordTable(valueList);  //그래프 아래 Record 리스트 생성
             }
         }
     }
 
-    private void showDeleteDialog(final long idToDelete) {
+    private void showDeleteDialog(final long idToDelete) {  //그래프 밑에 나와있는 record table에 X표시 관련(data delete)
 
         DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
             switch (which) {
-                case DialogInterface.BUTTON_POSITIVE:
-                    mBodyMeasureDb.deleteMeasure(idToDelete);
-                    refreshData();
+                case DialogInterface.BUTTON_POSITIVE:  //버튼 클릭
+                    mBodyMeasureDb.deleteMeasure(idToDelete);  //DB에서 데이터 삭제
+                    refreshData();  //데이터를 지웠으니 다시 그래프 등등을 업데이트
                     Toast.makeText(getActivity(), getResources().getText(R.string.removedid) + " " + idToDelete, Toast.LENGTH_SHORT)
                         .show();
                     break;
@@ -262,7 +265,7 @@ public class BodyPartDetailsFragment extends Fragment {
                     break;
             }
         };
-
+        //한 번 더 물어보는 알림창
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMessage(getResources().getText(R.string.DeleteRecordDialog)).setPositiveButton(getResources().getText(R.string.global_yes), dialogClickListener)
             .setNegativeButton(getResources().getText(R.string.global_no), dialogClickListener).show();
